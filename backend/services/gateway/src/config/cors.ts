eed6384ef1232d0d72aa2e44d362f0dc61342747
jmougel <jmougel@Inception.myguest.virtualbox.org>
2025-05-15 18:28:29 +0200

    fix: fix bad syntax frontend/src/pong/game/scenes/controlsManager.ts -> frontend/src/pong/game/scenes/ControlsManager.ts

--- backend/services/gateway/src/config/cors.ts
+++ backend/services/gateway/src/config/cors.ts
@@ -1,5 +1,6 @@
 export const corsConfig = {
-  origin: 'https://localhost:3000',
+  // origin: 'https://localhost:3000',
+  origin: 'http://localhost:3000',
   methods: ['GET', 'POST', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
