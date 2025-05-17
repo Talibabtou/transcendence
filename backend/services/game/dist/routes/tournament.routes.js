a79758caf4da48710400835747cf17fa06d6bcf6
Bboissen <33459312+Bboissen@users.noreply.github.com>
2025-05-17 21:27:55 +0200

    fix: removed dist / db folders

--- backend/services/game/dist/routes/tournament.routes.js
+++
@@ -1,6 +0,0 @@
-import { getTournament, getTournaments, getFinalMatches } from '../controllers/tournament.controller.js';
-export default async function tournamentRoutes(fastify) {
-    fastify.get('/tournament/:id', getTournament);
-    fastify.get('/tournaments', getTournaments);
-    fastify.get('/tournament/:id/final', getFinalMatches);
-}
--- backend/services/gateway/dist/config/cors.js
+++
@@ -1,7 +0,0 @@
-export const corsConfig = {
-    origin: 'http://localhost:3000',
-    // origin: 'https://localhost:3000',
-    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-};
