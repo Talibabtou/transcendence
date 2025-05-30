e2013a28c2fd2292dcc87215400561906e8c5227
Bboissen <33459312+Bboissen@users.noreply.github.com>
2025-05-30 18:22:57 +0200

    feat: remove commented code

--- frontend/src/website/scripts/services/db.ts
+++ frontend/src/website/scripts/services/db.ts
@@ -65,11 +65,8 @@ export class DbService {
 	 * @returns Promise resolving to the parsed response data of type T
 	 */
 	private static async handleApiResponse<T>(response: Response): Promise<T> {
-		// if (response.status === 404 && response.url.includes('friends/check')) {
-		// 	return Promise.resolve(null as unknown as T);
-		// }
-
 		if (response.status >= 400) {
+			console.log('response', response.url);
 			const errorData = await response.json();
 			if (response.status === 403) {
 				const { appState } = await import('../utils/app-state');
